3b5a204d5629256d9aa089597ea2ff2119c4561b
Rohitdutt <parsairohit2@gmail.com>
2020-09-08 12:10:28 +0530

    changed state onChange event

--- src/signInSignUp/signUp/signUp.js
+++ src/signInSignUp/signUp/signUp.js
@@ -1,52 +1,84 @@
-import React from "react";
+import React, {useState} from "react";
 import "./signUp.scss";
 import {Form , Col , Button} from "react-bootstrap";
 import {Link} from "react-router-dom";
 
-const signUp = () => {
-    // const [userName , setUserName] = useState(null);
-    //
-    // const handleChange = (event) = {
-    //     setUserName(event.target.value);
-    // }
+const SignUp = () => {
+    const [user , useUser] = useState({
+        userName:null,
+        mobileNo:null,
+        email:null,
+        password:null,
+    });
 
-    return (
-        <div className="signUp">
-            <h2>Sign Up</h2>
-            <br/>
-            <div className="form-area">
-                <Form>
-                    <Form.Group as={Col} controlId="formGridName">
-                        <Form.Label>Your Name</Form.Label>
-                        <Form.Control type="text" placeholder="Enter Your Name" required/>
-                    </Form.Group>
-                    <Form.Group as={Col} controlId="formGridMobile">
-                        <Form.Label>Your Mobile Number</Form.Label>
-                        <Form.Control type="email" placeholder="Enter Mobile Number" required/>
-                    </Form.Group>
-                    <Form.Group as={Col} controlId="formGridEmail">
-                        <Form.Label>Your Email</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" required/>
-                    </Form.Group>
-                    <Form.Group as={Col} controlId="formGridPassword">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="It Should be Top Secret!" required/>
-                    </Form.Group>
-                    <Form.Group as={Col} controlId="formGridPassword">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Confirm Password" required/>
-                    </Form.Group>
-                </Form>
-                <div className={"form-submission"}>
-                    <Button variant="outline-dark">Sign Up</Button>
-                    <p>Already a Customer , Click below</p>
-                    <Link to={"/signIn"}>
-                        <Button variant="link">Sign In</Button>
-                    </Link>
-                </div>
+    const HandleUserName = (e) =>{
+        useUser({
+            ...user,
+            userName:e.target.value});
+        console.log(user)
+    }
+
+    const HandleEmail = (e) =>{
+        useUser({
+            ...user,
+            email:e.target.value});
+        console.log(user)
+    }
+
+    const HandleMobileNo = (e) =>{
+        useUser({
+            ...user,
+            mobileNo:e.target.value});
+        console.log(user)
+    }
+
+    const HandlePassword = (e) =>{
+        useUser({
+            ...user,
+            password:e.target.value});
+        console.log(user)
+    }
+
+    const MatchPassword = () =>{
+
+
+    }
+
+    return <div className="signUp">
+        <h2>Sign Up</h2>
+        <br/>
+        <div className="form-area">
+            <Form>
+                <Form.Group as={Col} controlId="formGridName">
+                    <Form.Label>Your Name</Form.Label>
+                    <Form.Control type="text" placeholder="Enter Your Name" required onChange={HandleUserName.bind(this)}/>
+                </Form.Group>
+                <Form.Group as={Col} controlId="formGridMobile">
+                    <Form.Label>Your Mobile Number</Form.Label>
+                    <Form.Control type="text" placeholder="Enter Mobile Number" required onChange={HandleMobileNo.bind(this)}/>
+                </Form.Group>
+                <Form.Group as={Col} controlId="formGridEmail">
+                    <Form.Label>Your Email</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" required onChange={HandleEmail.bind(this)}/>
+                </Form.Group>
+                <Form.Group as={Col} controlId="formGridPassword" onChange={HandlePassword.bind(this)}>
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="It Should be Top Secret!" required />
+                </Form.Group>
+                <Form.Group as={Col} controlId="formGridConfirmPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Confirm Password" required/>
+                </Form.Group>
+            </Form>
+            <div className={"form-submission"}>
+                <Button variant="outline-dark" onClick={()=>{window.alert(user.email)}}>Sign Up</Button>
+                <p>Already a Customer , Click below</p>
+                <Link to={"/signIn"}>
+                    <Button variant="link">Sign In</Button>
+                </Link>
             </div>
         </div>
-    );
+    </div>;
 }
 
-export default signUp;
+export default SignUp;

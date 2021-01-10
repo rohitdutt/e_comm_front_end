f17b769eb8ef06dc3abfbdc19460225d350b7df0
Rohitdutt <parsairohit2@gmail.com>
2021-01-10 22:53:41 +0530

    pagination done

--- src/products/products.js
+++ src/products/products.js
@@ -2,13 +2,13 @@ import React from "react";
 import "./products.scss";
 import {Link} from "@reach/router";
 
-const Products = ({productId , productName , sellingPrice}) => {
+const Products = ({productId , productName }) => {
     return(
         <Link to={`/product/${productId}`}>
             <div className={"products"}>
                 <div className={"image"}/>
                 <h2>{productName}</h2>
-                <p>{sellingPrice} Rs.</p>
+                <p> Rs.</p>
             </div>
         </Link>
     )
--- src/products/productsList/productsList.js
+++ src/products/productsList/productsList.js
@@ -2,40 +2,79 @@ import React, {useEffect, useState} from "react";
 import "./productsList.scss";
 import Products from "../products";
 import Axios from "axios";
+// import Pagination from "../../Pagination/Pagination";
+// import {Pagination} from "semantic-ui-react";
+import Pagination from "react-js-pagination";
+
 
 const ProductsList = ({subCategoryId}) =>{
 
     const [products , setProducts] = useState([]);
-    const [productPrice , setProductPrice] = useState([]);
+    const [isLoading , setIsLoading] = useState(true);
+    const [currentPage , setCurrentPage] = useState(1);
+    const [productPerPage ] = useState(12);
+
+    // useEffect (()=>{
+    //     setIsLoading(true);
+    //     Axios.get(`http://localhost:8080/api/v1/product/product-sub-category/${subCategoryId}`)
+    //            .then(response => {
+    //                     setProducts(response.data)
+    //                     console.log("got products");
+    //                     setIsLoading(false);
+    //                 }
+    //             ).catch(error => {
+    //             console.log(error)
+    //         });
+    // },[subCategoryId]);
 
     useEffect (()=>{
-        console.log(subCategoryId)
-        Axios.get(`http://localhost:8080/api/v1/product/product-sub-category/${subCategoryId}`)
-               .then(response => {
-                        setProducts(response.data)
-                        Axios.get(`http://localhost:8080/api/v1/product/product-price-product-id/${response.data.productId}`)
-                            .then(r =>{
-                                const data = r.data;
-                                setProducts(data)
-                            })
-                    console.log(products);
-                    }
-                ).catch(error => {
-                console.log(error)
-            });
-    },[products, subCategoryId]);
+        const fetchProduct = async () => {
+            setIsLoading(true);
+            const res = await Axios.get(`http://localhost:8080/api/v1/product/product-sub-category/${subCategoryId}`);
+            setProducts(res.data);
+            setIsLoading(false);
+        }
+        fetchProduct().then(
+            console.log("Stop scrapping my website")
+        );
+    },[subCategoryId]);
+
+    console.log(products);
+
+    if (isLoading) {
+        return (
+            <div className={"loading"}>
+                <div className="progress">
+                    <div className="indeterminate"/>
+                </div>
+            </div>
+        )
+    }
+
+    const indexOfLastProduct = currentPage * productPerPage;
+    const indexOfFirstProduct = indexOfLastProduct - productPerPage;
+    const currentProducts = products.slice(indexOfFirstProduct , indexOfLastProduct);
 
     return(
-              <div className="ui grid">
-              {
-                  products.map((product) =>(
-                      <div className="four wide column">
+        <div className="ui grid">
+            {
+                currentProducts.map((product) => (
+                    <div className="four wide column">
                         <Products className={"product"} key={product.productId} productId={product.productId}
-                                productName={product.productName} sellingPrice={product}/>
-                      </div>
-                  ))
-              }
-              </div>
+                                  productName={product.productName}/>
+                    </div>
+                ))
+            }
+            <Pagination
+                activePage={currentPage}
+                itemsCountPerPage={12}
+                totalItemsCount={products.length}
+                onChange={(pageNumber)=>{
+                    console.log(`active page is ${currentPage}`);
+                    setCurrentPage(pageNumber)
+                }}
+            />
+        </div>
     )
 }
 

285f57f37fdc5a6b2ca6490126140d90c9aa705f
Rohitdutt <parsairohit2@gmail.com>
2021-02-20 23:12:34 +0530

    changed product page

--- src/products/productsList/productsList.js
+++ src/products/productsList/productsList.js
@@ -19,6 +19,7 @@ const ProductsList = ({subCategoryId}) =>{
             setProducts(res.data);
             setIsLoading(false);
         }
+        fetchProduct();
     },[subCategoryId]);
 
     console.log(products);

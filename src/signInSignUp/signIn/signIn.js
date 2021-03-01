0c456a95a778a781f1367d1e99153b8af59ee1ce
Rohitdutt <parsairohit2@gmail.com>
2021-03-01 23:17:47 +0530

    changes in request login

--- src/signInSignUp/signIn/signIn.js
+++ src/signInSignUp/signIn/signIn.js
@@ -1,34 +1,54 @@
-import React from "react";
+import React, {useState} from "react";
 import "./signIn.scss";
-import {Link} from "@reach/router";
-import {Button} from "semantic-ui-react";
+import {Link, navigate} from "@reach/router";
+import axios from "axios";
 
-const signIn = () =>(
-    <div className="signIn">
-        <h2>Sign In</h2>
-        <br/>
-        <div className="row">
-            <form className="col s12">
-                <div className="row">
-                    <div className="input-field col s12">
-                        <input id="email" type="email" className="validate"/>
-                        <label htmlFor="email">Email</label>
+const SignIn =()=>{
+
+    const [email , setEmail] = useState("");
+    const [password , setPassword] = useState("");
+
+    return(
+        <div className="signIn">
+            <h2>Sign In</h2>
+            <br/>
+            <div className="row">
+                <form className="col s12">
+                    <div className="row">
+                        <div className="input-field col s12">
+                            <input id="email" type="email" className="validate" onChange={(e)=>{
+                                setEmail(e.target.value);
+                            }}/>
+                            <label htmlFor="email">Email</label>
+                        </div>
                     </div>
-                </div>
-                <div className="row">
-                    <div className="input-field col s12">
-                        <input id="password" type="password" className="validate"/>
-                            <label htmlFor="password">Password</label>
+                    <div className="row">
+                        <div className="input-field col s12">
+                            <input id="password" type="password" className="validate" onChange={(e)=>{
+                                setPassword(e.target.value);
+                            }}/>
+                                <label htmlFor="password">Password</label>
+                        </div>
                     </div>
-                </div>
-            </form>
+                </form>
+            </div>
+            <button className="waves-effect waves-teal btn-flat" onClick={async ()=>{
+                const response = await axios.post("http://localhost:5001/api/login",{
+                    email : email,
+                    password : password
+                });
+                console.log(response.data['accessToken'])
+                localStorage.setItem('user',response.data['accessToken']);
+                if(response.status === 200){
+                    await navigate('/')
+                }
+            }}>Sign In</button>
+            <p>Haven't Signed Up yet , Click below</p>
+            <Link to={"/signUp"}>
+                <button className="waves-effect waves-teal btn-flat">Sign Up</button>
+            </Link>
         </div>
-        <button className="waves-effect waves-teal btn-flat">Sign In</button>
-        <p>Haven't Signed Up yet , Click below</p>
-        <Link to={"/signUp"}>
-            <button className="waves-effect waves-teal btn-flat">Sign Up</button>
-        </Link>
-    </div>
-);
+    );
+};
 
-export default signIn;
+export default SignIn;
--- src/signInSignUp/signUp/signUp.js
+++ src/signInSignUp/signUp/signUp.js
@@ -1,103 +1,103 @@
-import React, {Component} from "react";
+import React, { useState} from "react";
 import "./signUp.scss";
-import {Link} from "@reach/router";
+import {Link , navigate} from "@reach/router";
+import axios from "axios";
 
-class SignUp extends Component {
+const SignUp = (props) => {
+
+    const [firstName , setFirstName] = useState("");
+    const [lastName , setLastName] = useState("");
+    const [mobileNo , setMobileNo] = useState("")
+    const [email , setEmail] = useState("");
+    const [password , setPassword] = useState("");
+    const [reEnterPassword , setReEnterPassword] = useState("");
+    // const [passwordMatched , setPasswordMatched] = useState(true);
+    // const [passLength ,setPassLength] = useState(false);
 
 
-    constructor(props) {
-        super(props);
-        this.state={
-                userName: null,
-                mobileNo: null,
-                email: null,
-                password:null,
-                reEnterPassword: null,
-                passwordMatched: true,
-                passLength:false
-        }
-    }
-    HandleUserName = (e) => {
-        this.setState({userName:e.target.value})
-        console.log(this.state.userName)
-    }
-
-    HandleMobileNo = (e) => {
-        this.setState({mobileNo:e.target.value})
+    const handleMobileNo = (e) => {
+        setMobileNo(mobileNo + e.target.value)
         console.log(this.state)
     }
 
-    HandleEmail = (e) => {
-        this.setState({email:e.target.value})
-        console.log(this.state.email)
-    }
-
-    HandlePassword = (e) => {
-        this.setState({password:e.target.value})
-        console.log(this.state.password)
-    }
-
-    HandleReEnterPassword = (e) => {
-        this.setState({reEnterPassword:e.target.value})
-        console.log(this.state.reEnterPassword)
-    }
-
-
-    MatchPassword = () => {
-        const t1=this.state.password;
-        const t2=this.state.reEnterPassword;
-        let t3;
-        t3 = t1 === t2;
-        this.setState({passwordMatched: t3});
-
-    }
-
-    render() {
-        return (
-            <div className="signUp">
-                <h2>Sign Up</h2>
-                <br/>
-                    <div className="row">
-                         <form className="col s12">
-                            <div className="row">
-                                <div className="input-field col s6">
-                                    <input id="first_name" type="text" className="validate"/>
-                                        <label htmlFor="first_name">First Name</label>
-                                </div>
-                                <div className="input-field col s6">
-                                    <input id="last_name" type="text" className="validate"/>
-                                        <label htmlFor="last_name">Last Name</label>
-                                </div>
+    return (
+        <div className="signUp">
+            <h2>Sign Up</h2>
+            <br/>
+                <div className="row">
+                     <form className="col s12">
+                        <div className="row">
+                            <div className="input-field col s6">
+                                <input id="first_name" type="text" className="validate" onChange={(e)=>{
+                                    setFirstName(e.target.value);
+                                    console.log(firstName);
+                                }}/>
+                                    <label htmlFor="first_name">First Name</label>
                             </div>
-                            <div className="row">
-                                <div className="input-field col s12">
-                                    <input id="email" type="email" className="validate"/>
-                                        <label htmlFor="email">Email</label>
-                                </div>
+                            <div className="input-field col s6">
+                                <input id="last_name" type="text" className="validate" onChange={(e)=>{
+                                    setLastName(e.target.value);
+                                    console.log(lastName)
+                                }}/>
+                                    <label htmlFor="last_name">Last Name</label>
                             </div>
-                             <div className="row">
-                                 <div className="input-field col s6">
-                                     <input id="password" type="password" className="validate"/>
-                                     <label htmlFor="password">Password</label>
-                                 </div>
-                                 <div className="input-field col s6">
-                                     <input id="confirm_password" type="password" className="validate"/>
-                                     <label htmlFor="confirm_password">Confirm Password</label>
-                                 </div>
+                        </div>
+                        <div className="row">
+                            <div className="input-field col s12">
+                                <input id="email" type="email" className="validate" onChange={(e)=>{
+                                        setEmail(e.target.value );
+                                        console.log(email)
+                                    }
+                                }/>
+                                    <label htmlFor="email">Email</label>
+                            </div>
+                        </div>
+                         <div className="row">
+                             <div className="input-field col s6">
+                                 <input id="password" type="password" className="validate" onChange={(e)=>{
+                                     setPassword( e.target.value);
+                                     console.log(password);
+                                 }}/>
+                                 <label htmlFor="password">Password</label>
                              </div>
-                             <div className={"form-bottom"}>
-                                 <a href={"/"} className="waves-effect waves-teal btn-flat" onClick={this.MatchPassword}>Sign Up</a>
-                                 <div>
-                                     <p>Already a Customer , Click below</p>
-                                     <Link to={"/signIn"}>
-                                         <a href={"signIn"} className="waves-effect waves-teal btn-flat">Sign In</a>
-                                     </Link>
-                                 </div>
+                             <div className="input-field col s6">
+                                 <input id="confirm_password" type="password" className="validate" onChange={(e)=>{
+                                     setReEnterPassword(e.target.value);
+                                     console.log(reEnterPassword);
+                                 }}/>
+                                 <label htmlFor="confirm_password">Confirm Password</label>
                              </div>
-                        </form>
-                    </div>
-            </div>
+                         </div>
+                         <div className={"form-bottom"}>
+                             <a href={"#"} className="waves-effect waves-teal btn-flat" onClick={async ()=>{
+                                 console.log(password + "  " + reEnterPassword)
+                                 if(password === reEnterPassword){
+                                    const response = await axios.post("http://localhost:5001/api/signUp",{
+                                        email: email,
+                                        password: password
+                                    })
+                                     if(response.status !== 201){
+                                         window.alert("no");
+                                         await navigate("/");
+                                         console.log("-------------------")
+                                     }else{
+                                         window.alert("success");
+                                         await navigate("/");
+                                     }
+
+                                 }
+                             }
+                             }>Sign Up</a>
+                             <div>
+                                 <p>Already a Customer , Click below</p>
+                                 <Link to={"/signIn"}>
+                                     <a href={"signIn"} className="waves-effect waves-teal btn-flat">Sign In</a>
+                                 </Link>
+                             </div>
+                         </div>
+                    </form>
+                </div>
+        </div>
     )
-}
 }
 export default SignUp;
